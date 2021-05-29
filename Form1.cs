d63b83939e65ccbdf700e857665eaae122f33c49
SED4906 <37986786+SED4906@users.noreply.github.com>
2021-05-29 12:44:27 -0700

    yes

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ClipboardMonitoringSED
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Places the given window in the system-maintained clipboard format listener list.
+        /// </summary>
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        [return: System.Runtime.InteropServices.MarshalAs(UnmanagedType.Bool)]
+        static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Removes the given window from the system-maintained clipboard format listener list.
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Sent when the contents of the clipboard have changed.
+        /// </summary>
+        private const int WM_CLIPBOARDUPDATE = 0x031D;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public Form1()
+        {
+            AddClipboardFormatListener(this.Handle);
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Windows\Media\ding.wav");
+                player.Play();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RemoveClipboardFormatListener(this.Handle);     // Remove our window from the clipboard's format listener list.
+        }
+    }
+}
